e8b6129a6b29ec501137720ed6e8610e2591270b
mv_motionai <mv_motionai@9b2b60d8-dc1a-0410-86e4-9f464c1a20d8>
2006-11-15 04:52:48 +0000

    version 00-01-18 - ubuntu binaries (i386) included - Code::Blocks ubuntu projects included

--- mvMotionAI/examples-src/Camera.cpp
+++ mvMotionAI/examples-src/Camera.cpp
@@ -28,14 +28,14 @@
 void Camera::applyTranslation()
 {
    glTranslatef(-xPosition,-yPosition,-zPosition);
-};
+}
 
 void Camera::applyRotation()
 {
    glRotatef(xRotate,1.0f,0.0f,0.0f);
    glRotatef(yRotate,0.0f,1.0f,0.0f);
    glRotatef(zRotate,0.0f,0.0f,1.0f);
-};
+}
 
 /*
    float xPosition;
@@ -50,17 +50,17 @@ Camera::Camera()
 {
    resetCamera();
    rotationMethod = CAMERA_AROUND;
-};
+}
 
 void Camera::setCameraAround()
 {
    rotationMethod = CAMERA_AROUND;
-};
+}
 
 void Camera::setCameraOn()
 {
    rotationMethod = CAMERA_ON;
-};
+}
 
 Camera::Camera(float x, float y, float z)
 {
@@ -70,7 +70,7 @@ Camera::Camera(float x, float y, float z)
    xRotate = 0.0f;
    yRotate = 0.0f;
    zRotate = 0.0f;
-};
+}
 
 void Camera::resetCamera()
 {
@@ -80,14 +80,14 @@ void Camera::resetCamera()
    xRotate = 0.0f;
    yRotate = 0.0f;
    zRotate = 0.0f;
-};
+}
 
 void Camera::setCamera(float x, float y,float z)
 {
    xPosition = x;
    yPosition = y;
    zPosition = z;
-};
+}
 
 void Camera::apply()
 {
@@ -101,102 +101,102 @@ void Camera::apply()
       applyRotation();
       applyTranslation();
    }
-};
+}
 
 // add rotation
 void Camera::addXRotation(float xAngle)
 {
    xRotate += xAngle;
-};
+}
 
 void Camera::addYRotation(float yAngle)
 {
    yRotate += yAngle;
-};
+}
 
 void Camera::addZRotation(float zAngle)
 {
    zRotate += zAngle;
-};
+}
 
 // add  Position
 void Camera::addXPosition(float x)
 {
    xPosition += x;
-};
+}
 
 void Camera::addYPosition(float y)
 {
    yPosition += y;
-};
+}
 
 void Camera::addZPosition(float z)
 {
    zPosition += z;
-};
+}
 
 // set rotation
 void Camera::setXRotation(float xAngle)
 {
    xRotate = xAngle;
-};
+}
 
 void Camera::setYRotation(float yAngle)
 {
    yRotate = yAngle;
-};
+}
 
 void Camera::setZRotation(float zAngle)
 {
    zRotate = zAngle;
-};
+}
 
 // set  Position
 void Camera::setXPosition(float x)
 {
    xPosition = x;
-};
+}
 
 void Camera::setYPosition(float y)
 {
    yPosition = y;
-};
+}
 
 void Camera::setZPosition(float z)
 {
    zPosition = z;
-};
+}
 
 // get rotation
 float Camera::getXRotation() const
 {
    return xRotate;
-};
+}
 
 float Camera::getYRotation() const
 {
    return yRotate;
-};
+}
 
 float Camera::getZRotation() const
 {
    return zRotate;
-};
+}
 
 // get position
 float Camera::getXPosition() const
 {
    return xPosition;
-};
+}
 
 float Camera::getYPosition() const
 {
    return yPosition;
-};
+}
 
 float Camera::getZPosition() const
 {
    return zPosition;
-};
+}
 
 
