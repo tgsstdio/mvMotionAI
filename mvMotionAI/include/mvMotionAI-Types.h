0bf1bb1e1437bd6bf76af858aea1369a413a758f
mv_motionai <mv_motionai@9b2b60d8-dc1a-0410-86e4-9f464c1a20d8>
2006-11-19 03:22:38 +0000

    version 00-01-20 - changed libmvMotionAI to libmvMotion-mingw.a - added more Doxygen documentation - checked updated Code::Blocks workspace

--- mvMotionAI/include/mvMotionAI-Types.h
+++ mvMotionAI/include/mvMotionAI-Types.h
@@ -1,5 +1,8 @@
 /**
- *\file mvMotionAI-Types.h
+ * \file mvMotionAI-Types.h
+ *
+ * \brief defines the basic data types used in mvMotionAI
+ *
  * Copyright (c) 2006 David Young.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
@@ -20,22 +23,32 @@
  * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
  * DEALINGS IN THE SOFTWARE.
  *
+ * Log
+ *
+ * version     date
+ *
+ * 00-01-20    18/11/06  \li doxygen documentation added
  */
 #ifndef MV_MOTIONAI_TYPES_H_
 #define MV_MOTIONAI_TYPES_H_
 
-/**
- * changes the values of
- * data type in the system
- */
-
-/**
+/*
  * FLOATING POINT - used in Vec3 and other places
  */
 
 #ifndef MV_MOTIONAI_HIGHER_PRECISION_FLOATING_POINT
+/**
+ * \brief  flag for higher precision mvFloat data type
+ *
+ * default : uses float (32 bits / 4 bytes)
+ * higher precision : uses double (64 bits / 8 bytes)
+ * comment out the line below
+ * changes the float type to double
+ *
+ * i.e \#undef to //\#undef
+ */
 #define MV_MOTIONAI_HIGHER_PRECISION_FLOATING_POINT 1
-/** comment this line below changes the float type to double **/
+
 #undef MV_MOTIONAI_HIGHER_PRECISION_FLOATING_POINT
 #endif
 
@@ -45,16 +58,30 @@ typedef double mvFloat;
 typedef float mvFloat;
 #endif
 
-/**
+/*
  * INDEXES & COUNT - used in mvVec3 and other places
  */
 
+/**
+ * \brief mvIndex / mvCount data type flag to use integer (32 bits / 4 bytes)
+ */
 #define MV_MOTIONAI_INDEX_TYPE_INTEGER 0
+
+/**
+ * \brief mvIndex / mvCount data type flag to use short (16 bits / 2 bytes)
+ */
 #define MV_MOTIONAI_INDEX_TYPE_SHORT 1
+
+/**
+ * \brief mvIndex / mvCount data type flag to use long (64 bits / 8 bytes)
+ */
 #define MV_MOTIONAI_INDEX_TYPE_LONG 2
 
-// alter this value to change the type
+// alter this value MV_MOTIONAI_INDEX_TYPE to change the index type
 #ifndef MV_MOTIONAI_INDEX_TYPE
+/**
+ * \brief default mvIndex data type flag to use integer (int)
+ */
 #define MV_MOTIONAI_INDEX_TYPE MV_MOTIONAI_INDEX_TYPE_INTEGER
 #endif
 
@@ -69,8 +96,22 @@ typedef int mvIndex;
 typedef int mvCount;
 #endif
 
+/**
+ * \brief offset value to index value in mvMotionAI
+ *
+ * first item in list is 1, last item is n
+ * (unlike C, where first item is 0 and last item is n - 1 )
+ */
 static const mvIndex MV_OFFSET_TO_INDEX = -1;
-static const mvIndex MV_NO_CURRENT_INDEX = -0;
+
+/**
+ * \brief index value returned if error as occured
+ */
+static const mvIndex MV_NO_CURRENT_INDEX = 0;
+
+/**
+ * \brief maximum array length required for set and retrieving parameters
+ */
 static const mvCount MV_MAX_NO_OF_PARAMETERS = 3;
 
 #endif
