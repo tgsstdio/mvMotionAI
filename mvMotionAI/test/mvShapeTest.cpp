1016c4f5e6651ffc62040a4e379d55992564811e
David Young <tgsstdio@gmail.com>
2007-02-06 14:22:50 +0000

    version 00-01-44 LIB WILL NOT COMPILE - CPPUNIT TEST CASE IMPLEMENTATION     - implemented copy of tutorial in CPPUnit docs     - new Code::Blocks project for static library in minGW     - new Code::Blocks project for mvVec3

---
+++ mvMotionAI/test/mvShapeTest.cpp
@@ -0,0 +1,34 @@
+#include <cstdlib>
+#include <cstdio>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/XmlOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/ui/text/TestRunner.h>
+
+/**
+ * Money Test Suite example from CPPUnit Cookbook
+ */
+int main(void)
+{
+  // Get the top level suite from the registry
+  CppUnit::Test *suite = CppUnit::TestFactoryRegistry::getRegistry().makeTest();
+
+  // Adds the test to the list of test to run
+  CppUnit::TextUi::TestRunner runner;
+  runner.addTest( suite );
+
+  // Change the default outputter to a compiler error format outputter
+  runner.setOutputter( new CppUnit::CompilerOutputter( &runner.result(),
+                                                       std::cerr ) );
+  // Run the tests.
+  bool wasSucessful = runner.run();
+
+  // Return error code 1 if the one of test failed.
+  if (wasSucessful)
+  {
+      puts("HELLO");
+  }
+
+  return wasSucessful ? 0 : 1;
+}
+
---
+++ mvMotionAI/test/mvVec3Test.h
@@ -0,0 +1,44 @@
+/**
+ * CPPUnit Template Test Suite
+ */
+#ifndef MVVEC3_HEADER_H
+#define MVVEC3_HEADER_H
+#include <cppunit/extensions/HelperMacros.h>
+
+class mvVec3Test : public CppUnit::TestFixture
+{
+   CPPUNIT_TEST_SUITE( mvVec3Test );
+   CPPUNIT_TEST(  testConstructor );
+   //CPPUNIT_TEST( testEqual );
+   CPPUNIT_TEST( testAdd );
+   CPPUNIT_TEST( testSubtract );
+   CPPUNIT_TEST( testMultiply );
+   CPPUNIT_TEST( testDivide );
+   CPPUNIT_TEST( testSigned );
+   //CPPUNIT_TEST_EXCEPTION( testAddThrow, IncompatibleMoneyError );
+   CPPUNIT_TEST_SUITE_END();
+
+   public:
+      void setUp();
+      void testConstructor();
+
+      void testAdd();
+      void testSubtract();
+      void testDivide();
+      void testMultiply();
+      void testSigned();
+
+      // TODO: new tests to do
+      void testLength();
+      void testNormalise();
+      void testDotProduct();
+      void testCosineMeasure();
+      void testCrossProduct();
+      void testEquality();
+      void testAssigned();
+      void testSetAndReset();
+
+      void tearDown();
+};
+
+#endif
