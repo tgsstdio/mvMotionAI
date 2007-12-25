dce9fdf0fbbc14db3884929cab3426fdbd26e207
David Young <tgsstdio@gmail.com>
2007-12-25 12:27:15 +0000

    version 00-02-50+ adding new experimental C::B project for C++ bitfields in mvMotionAI classes

---
+++ mvMotionAI/test/mvBitFieldsUnitTest.h
@@ -0,0 +1,28 @@
+#ifndef MVBITFIELDSUNITTEST_H_INCLUDED
+#define MVBITFIELDSUNITTEST_H_INCLUDED
+
+#include <cppunit/extensions/HelperMacros.h>
+
+#define TESTFIXTURE_CLASS_NAME mvBitFieldUnitTest
+
+class TESTFIXTURE_CLASS_NAME : public CppUnit::TestFixture
+{
+   CPPUNIT_TEST_SUITE( TESTFIXTURE_CLASS_NAME );
+   CPPUNIT_TEST(testBody);
+   CPPUNIT_TEST(testBehaviourResult);
+   CPPUNIT_TEST(testForceResult);
+   CPPUNIT_TEST_SUITE_END();
+
+   public:
+      void setUp();
+      void tearDown();
+
+      void testBody();
+      void testBehaviourResult();
+      void testForceResult();
+      void testGetParameter();
+};
+
+#undef TESTFIXTURE_CLASS_NAME
+
+#endif // MVBITFIELDSUNITTEST_H_INCLUDED
