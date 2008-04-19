48a5a4c9591a8e1bea7ad7e7dab766065b694ee0
mv_motionai <mv_motionai@9b2b60d8-dc1a-0410-86e4-9f464c1a20d8>
2008-04-19 04:03:36 +0000

    version 2.62+ added quaternion class

---
+++ mvMotionAI/mvLightfeather Conversion/mvLFQuaternion.cpp
@@ -0,0 +1,54 @@
+#include "mvLFQuaternion.h"
+
+/** @brief mvLFQuaternion
+  *
+  * @todo: document this function
+  */
+mvLFQuaternion::mvLFQuaternion(const mvVec3& rhs)
+{
+	lf::core::vector3df temp = rhs.components;
+	m_qtr.toEulerDegrees(temp);
+}
+
+/** @brief convertToVec3
+  *
+  * @todo: document this function
+  */
+mvVec3 mvLFQuaternion::convertToVec3() const
+{
+	mvVec3 temp;
+	temp.components = m_qtr.toEuler();
+	return temp;
+}
+
+mvLFQuaternion mvLFQuaternion::interpolate(mvFloat factor) const
+{
+	return mvLFQuaternion(m_qtr * factor);
+}
+
+void mvLFQuaternion::invert()
+{
+	m_qtr.makeInverse();
+}
+
+void mvLFQuaternion::composeProduct(const mvLFQuaternion& rhs)
+{
+	m_qtr *= rhs.m_qtr;
+}
+
+void mvLFQuaternion::setDelta(const mvLFQuaternion& lhs, const mvLFQuaternion& rhs)
+{
+	m_qtr = lhs.m_qtr;
+	m_qtr.makeInverse();
+	m_qtr *= rhs.m_qtr;
+}
+
+mvLFQuaternion::mvLFQuaternion(const lf::core::quaternion& rhs)
+{
+	m_qtr = rhs;
+}
+
+mvLFQuaternion::mvLFQuaternion()
+{
+
+}
